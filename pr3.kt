1a1c798bac298558c1537dcfcb760a4c4c139c01
vadniks <whatever@fake.some>
2023-09-14 23:21:39 +0300

    pr3

--- pr3.kt
+++ pr3.kt
@@ -2,11 +2,14 @@
 import io.reactivex.rxjava3.core.*
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.functions.Consumer
+import io.reactivex.rxjava3.functions.Function
 import io.reactivex.rxjava3.schedulers.Schedulers
 import org.reactivestreams.Publisher
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.random.Random
+import kotlin.reflect.KClass
 
 object P3T1 {
 
@@ -107,32 +110,51 @@ object P3T2 {
         .generated(intRandom) { println(count().blockingGet()) }
 
     private fun t221() {
-        val t1 = Schedulers.newThread()
-        val t2 = Schedulers.newThread()
+        val size = 10
+        val chars = Array(size) { Pair(Char::class, 'a') }
+        val ints = Array(size) { Pair(Int::class, 0) }
 
-        var c1 = 0
-        var c2 = 10
-
-        Flowable.merge(
-            Flowable.generate<Int> {
-                Thread.sleep(10)
-                if (c1 < 10)
-                    it.onNext(c1++)
-                else
-                    it.onComplete()
-            }.subscribeOn(t1),
-            Flowable.generate<Int> {
-                Thread.sleep(10)
-                if (c2 < 20)
-                    it.onNext(c2++)
-                else
-                    it.onComplete()
-            }.subscribeOn(t2)
-        ).subscribe {
-            println(it)
+        for (i in 0 until size) {
+            chars[i] = Pair(Char::class, /*Random.nextInt('a'.code, 'z'.code)*/('a'.code + i).toChar())
+            ints[i] = Pair(Int::class, i/*Random.nextInt(0, 100)*/)
         }
 
-        Executors.newCachedThreadPool().submit {  }
+        val executor = Executors.newSingleThreadExecutor()
+        executor.submit {}
+
+        val t1 = Schedulers.newThread()
+        val t2 = Schedulers.newThread()
+        val t3 = Schedulers.from(executor)
+
+        val queueA = ArrayDeque<Char>()
+        val queueB = ArrayDeque<Int>()
+        val which = AtomicBoolean(false)
+
+        Observable.merge<Pair<KClass<*>, Any>>(
+            Observable.fromArray(*chars).subscribeOn(t1).observeOn(t3),
+            Observable.fromArray(*ints).subscribeOn(t2).observeOn(t3)
+        ).observeOn(t3).doOnComplete { t3.shutdown() }.subscribe { item ->
+            if (item.first == Char::class) queueA.addLast(item.second as Char)
+            else queueB.addLast(item.second as Int)
+
+            if (!which.get()) {
+                var next: Char?
+                if (queueA.removeFirstOrNull().also { next = it } != null)
+                    println(next)
+                else
+                    return@subscribe
+                which.set(true)
+            } else {
+                var next: Int?
+                if (queueB.removeFirstOrNull().also { next = it } != null)
+                    println("\t" + next)
+                else
+                    return@subscribe
+                which.set(false)
+            }
+        }
+
+        executor.awaitTermination(10, TimeUnit.SECONDS)
     }
 }
 
