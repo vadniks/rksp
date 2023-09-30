c5f5739c4e6d5d7f97fedff9015fb2bc9f147ecf
vadniks <whatever@fake.some>
2023-09-30 14:56:49 +0300

    pr4

--- pr4/server/src/main/kotlin/com/example/rsocket/Repository.kt
+++ pr4/server/src/main/kotlin/com/example/rsocket/Repository.kt
@@ -4,7 +4,12 @@ package com.example.rsocket
 import org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON
 import org.springframework.context.annotation.Scope
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-@Scope(SCOPE_SINGLETON) interface Repository : JpaRepository<Component, Int>
+@Scope(SCOPE_SINGLETON) interface Repository : JpaRepository<Component, Int> {
+
+    @Query("truncate table components restart identity")
+    fun prune()
+}
---
+++ pr4/server/src/test/kotlin/com/example/rsocket/RsocketApplicationTests.kt
@@ -0,0 +1,104 @@
+
+package com.example.rsocket
+
+import kotlinx.coroutines.runBlocking
+import org.aspectj.lang.annotation.After
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.rsocket.server.LocalRSocketServerPort
+import org.springframework.messaging.rsocket.RSocketRequester
+import org.springframework.messaging.rsocket.RSocketStrategies
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler
+import org.springframework.messaging.rsocket.connectTcpAndAwait
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
+
+@SpringBootTest
+class RsocketApplicationTests(
+    builder: RSocketRequester.Builder,
+    @LocalRSocketServerPort port: Int,
+    rSocketStrategies: RSocketStrategies,
+    repository: Repository
+) {
+    private lateinit var rSocketRequester: RSocketRequester
+
+    init {
+        repository.prune()
+
+        rSocketRequester = builder
+            .setupRoute("connect")
+            .setupData(System.currentTimeMillis())
+            .rsocketConnector { it.acceptor(RSocketMessageHandler.responder(rSocketStrategies, Any())) }
+            .run { runBlocking { connectTcpAndAwait("localhost", port) } }
+    }
+
+    private fun assert(condition: Boolean) = assertDoesNotThrow { if (!condition) throw AssertionError() }
+
+    private inline fun <T : Any> T.doAction(crossinline action: (T) -> Unit) = action(this)
+
+    @Order(1)
+    @Test
+    fun test_fireNForget_addComponent() = rSocketRequester
+        .route("addOne")
+        .data(Message(false, Component(Component.Type.CPU, "cpu1", 1).serialized))
+        .retrieveMono(Void::class.java)
+        .doAction {
+            StepVerifier
+                .create(it)
+                .verifyComplete()
+        }
+
+    @Order(2)
+    @Test
+    fun test_requestResponse_getComponent() = rSocketRequester
+        .route("getOne")
+        .data(Message(false, "1"))
+        .retrieveMono(Message::class.java)
+        .doAction { message ->
+            StepVerifier
+                .create(message)
+                .consumeNextWith {
+                    assert(!it.stream)
+                    assert(it.payload != null)
+                    assert(it.index == 0)
+
+                    Component.deserialized(it.payload!!).apply {
+                        assert(type == Component.Type.CPU)
+                        assert(name == "cpu1")
+                        assert(cost == 1)
+                        assert(id == 1)
+                    }
+                }
+                .verifyComplete()
+        }
+
+    @Order(3)
+    @Test
+    fun test_channel_addComponents() = rSocketRequester
+        .route("addSeveral")
+        .data(Flux.fromIterable(ArrayList<Message>().apply {
+            add(Message(true, Component(Component.Type.MOTHERBOARD, "mb1", 2).serialized))
+            add(Message(true, Component(Component.Type.RAM, "ram1", 3).serialized, 1))
+            add(Message(true, Component(Component.Type.GPU, "gpu1", 4).serialized, 2))
+        }))
+        .retrieveFlux(Message::class.java)
+        .doAction {
+            
+        }
+
+    @Order(3)
+    @Test
+    fun testStream_getAll() {
+//        StepVerifier
+//            .create(rSocketRequester
+//                .route("getAll")
+//                .retrieveFlux(Message::class.java))
+//            .consumeNextWith {
+//                assert(it.stream)
+//                assert(it.payload != null)
+//            }
+//            .expectComplete()
+    }
+}
